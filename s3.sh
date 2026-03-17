f9360495e1a7a327568116d9a1d8d8bdbe54b2f2
himanshu pawar <himanshupawar@himanshus-MacBook-Air.local>
2026-03-18 00:55:32 +0530

    comments updated

--- s3.sh
+++ s3.sh
@@ -24,7 +24,7 @@ aws s3api put-public-access-block \
   BlockPublicAcls=true,IgnorePublicAcls=true,BlockPublicPolicy=true,RestrictPublicBuckets=true
 
 
-# # Create an S3 bucket with the specified name and tagss
+# # Create an S3 bucket with the specified name and tags
 # resource "aws_s3_bucket" "my_bucket" {
 #   bucket = var.bucket_name
 
