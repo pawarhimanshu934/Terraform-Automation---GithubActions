f6e77608add3414713f7198730351c1d27729d2d
pawarhimanshu934 <69887496+pawarhimanshu934@users.noreply.github.com>
2026-03-18 14:44:54 +0000

    code formatted done by terraform

--- output.tf
+++ output.tf
@@ -1,20 +1,20 @@
 output "vpc_id" {
   description = "Value of VPC ID"
-  value = aws_vpc.my_vpc.id
+  value       = aws_vpc.my_vpc.id
 
 }
 
 output "public_subnet_ids" {
   description = "List of public subnet IDs"
-  value = aws_subnet.public_subnet[*].id
+  value       = aws_subnet.public_subnet[*].id
 }
 
 output "load_balancer_DNS" {
-    description = "DNS name of the load balancer"
-    value = aws_lb.my_alb.dns_name
+  description = "DNS name of the load balancer"
+  value       = aws_lb.my_alb.dns_name
 }
 
 output "autoScaling_group-name" {
   description = "Name of autoscaling group"
-  value = aws_autoscaling_group.my_asg.name
+  value       = aws_autoscaling_group.my_asg.name
 }
